83642d79e0fb2546909b91e6b03e60341eff38e5
walterpinayasantos <56658949+walterpinayasantos@users.noreply.github.com>
2022-06-08 19:52:54 -0400

    Update 02.py
    Ejercicio 2 Curso Básico de Python

--- 02.py
+++ 02.py
@@ -1,4 +1,4 @@
-'''Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y
-luege muestre por consola el contenido de la variable'''
+'''Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable
+y luege muestre por consola el contenido de la variable'''
 saludo = "Hola Mundo!"
 print(saludo)
